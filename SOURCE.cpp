c1967a5d823d6929ea14f56ad40d8da43a794efe
Nguyễn Duy <nguyenhoanganhduy17022006@gmail.com>
2026-01-26 13:47:00 +0700

    Nguyen Hoang Anh Duy, da them lop User, Teacher, Student, Admin, va main

--- SOURCE.cpp
+++ SOURCE.cpp
@@ -42,6 +42,318 @@ public:
 		/////code//////
 	}
 };
+
+class User {
+private:
+	long long id;
+	int yearOfBirth;
+	string name, email, password;
+public:
+	User() {};
+	User(long long id, int yearOfBirth, string name, string email, string password) {
+		this->id = id;
+		this->yearOfBirth = yearOfBirth;
+		this->name = name;
+		this->email = email;
+		this->password = password;
+	}
+	friend istream& operator >> (istream& in, User& x) {
+		cout << "1.ID : ";
+		string idtmp;
+		getline(in, idtmp);
+		while (!employeeNumberCheck(idtmp)) {
+			cout << "Invalid ID, please enter again :";
+			getline(in, idtmp);
+		}
+		x.id = stoll(idtmp);
+		cout << "2.Name : ";
+		getline(in, x.name);
+		while (checkUserName(x.name) == false) {
+			cout << "Invalid name. Please enter again: ";
+			getline(in, x.name);
+		}
+		chuanHoaTen(x.name);
+		cout << "3. Email : ";
+		getline(in, x.email);
+		cout << "4. Password : ";
+		getline(in, x.password);
+		cout << "5. Year Of Birth : ";
+		in >> x.yearOfBirth;
+		while (checkUserYOB(x.yearOfBirth) == false) {
+			cout << "Invalid year of birth. Please enter again: ";
+			in >> x.yearOfBirth;
+		}
+		in.ignore();
+		idCheck.insert(x.id);
+		AccountData.insert({ x.email, x.password });
+		return in;
+	}
+	void printInformation() {
+		cout << "ID: " << id << endl;
+		cout << "Name: " << name << endl;
+		cout << "Email: " << email << endl;
+		cout << "Password: " << password << endl;
+		cout << "Year Of Birth: " << yearOfBirth << endl;
+	}
+	long long getID() {
+		return id;
+	}
+	int getYearOfBirth() {
+		return yearOfBirth;
+	}
+	string getName() {
+		return name;
+	}
+	string getEmail() {
+		return email;
+	}
+	string getPassword() {
+		return password;
+	}
+	void setID(long long id) {
+		this->id = id;
+	}
+	void setYearOfBirth(int yearOfBirth) {
+		this->yearOfBirth = yearOfBirth;
+	}
+	void setName(string name) {
+		this->name = name;
+	}
+	void setEmail(string email) {
+		this->email = email;
+	}
+	void setPassword(string password) {
+		this->password = password;
+	}
+	void login(string email, string password) {
+		if (AccountData.find(email) != AccountData.end() && AccountData[email] == password) {
+			cout << "Login successful !" << endl;
+		}
+		else if (AccountData.find(email) == AccountData.end()) {
+			cout << "Email not found !" << endl;
+		}
+		else {
+			cout << "Incorrect password !" << endl;
+		}
+	}
+	void logout() {
+		cout << "Logout successful !" << endl;
+	}
+};
+
+class Teacher : public User {
+private:
+	int employeeNumber;
+	string department, degree;
+public:
+	Teacher() {};
+	Teacher(long long id, int yearOfBirth, string name, string email, string password, int employeeNumber, string department, string degree)
+		: User(id, yearOfBirth, name, email, password) {
+		this->employeeNumber = employeeNumber;
+		this->department = department;
+		this->degree = degree;
+	}
+	friend istream& operator >> (istream& in, Teacher& x) {
+		in >> static_cast<User&>(x);
+		cout << "1. Employee Number : ";
+		string tmp;
+		getline(in, tmp);
+		while (!employeeNumberCheck(tmp)) {
+			cout << "Employee Number Invalid, please enter again : ";
+			getline(in, tmp);
+		}
+		x.employeeNumber = stoi(tmp);
+		cout << "2. Degree : ";
+		getline(in, x.degree);
+		cout << "3. Department : ";
+		getline(in, x.department);
+		return in;
+	}
+	void printInformation() {
+		cout << "ID: " << getID() << endl;
+		cout << "Name: " << getName() << endl;
+		cout << "Email: " << getEmail() << endl;
+		cout << "Password: " << getPassword() << endl;
+		cout << "Year Of Birth: " << getYearOfBirth() << endl;
+		cout << "Employee Number : " << employeeNumber << endl;
+		cout << "Degree : " << degree << endl;
+		cout << "Department : " << department << endl;
+	}
+	void setDegree(string degree) {
+		this->degree = degree;
+	}
+	void setEmployeeNumber(int employeeNumber) {
+		this->employeeNumber = employeeNumber;
+	}
+	void setDepartment(string department) {
+		this->department = department;
+	}
+	void makeQuiz(string title, string description) {
+
+	}
+	void deleteQuiz(string quizID) {
+
+	}
+	void setTimeLimit(string quizID, int timeLimit) {
+		////code////
+	}
+	void gradeAttempt() {
+		////code////
+	}
+};
+
+class Student : public User {
+private:
+	int studentNumber, year;
+	string major, studentAnswer;
+public:
+	Student() {};
+	Student(long long id, int yearOfBirth, string name, string email, string password, int studentNumber, string major,
+		int year) : User(id, yearOfBirth, name, email, password) {
+		this->studentNumber = studentNumber;
+		this->major = major;
+		this->year = year;
+	}
+	friend istream& operator >> (istream& in, Student& x) {
+		in >> static_cast<User&>(x);
+		cout << "Student Number : ";
+		string tmp;
+		getline(in, tmp);
+		while (!employeeNumberCheck(tmp)) {
+			cout << "Invalid Student Number, please enter again : ";
+			getline(in, tmp);
+		}
+		x.studentNumber = stoi(tmp);
+		cout << "Major (if exist, if don't, type ""nothing"" ";
+		getline(in, x.major);
+		cout << " Year : ";
+		in >> x.year;
+		while (checkUserYOB(x.year) == false) {
+			cout << "Year invalid, please enter again : ";
+			cin >> x.year;
+		}
+		in.ignore();
+		return in;
+	}
+	void printInformation() {
+		cout << "ID: " << getID() << endl;
+		cout << "Name: " << getName() << endl;
+		cout << "Email: " << getEmail() << endl;
+		cout << "Password: " << getPassword() << endl;
+		cout << "Year Of Birth: " << getYearOfBirth() << endl;
+		cout << "Student Number : " << studentNumber << endl;
+		cout << "Major : " << major << endl;
+		cout << "Year : " << year;
+	}
+
+	void SaveAnswer() {
+
+	}
+	void SummitAttempt() {
+
+	}
+	void ViewResult() {
+		////code////
+	}
+};
+class Admin : public User {
+private:
+	string BackupID;
+public:
+	Admin() {};
+	Admin(long long id, int yearOfBirth, string name, string email, string password, string BackupID)
+		: User(id, yearOfBirth, name, email, password) {
+		this->BackupID = BackupID;
+	}
+	void RestoreData(string backupID) {
+		////code////
+	}
+	void BackupData() {
+		////code////	
+	}
+};
+class Result {
+private:
+	string resultID;
+public:
+	Result() {};
+	Result(string resultID) {
+		this->resultID = resultID;
+	}
+	double CalculatePercentage(double maxScore) {
+		////code////
+	}
+	double CalculateGrade(double totalScore) {
+		////code////
+	}
+};
+
+int main()
+{
+	srand(time(NULL));
+	cout << "================================================" << endl;
+	cout << "	   QUIZ EXAMINATION SYSTEM " << endl;
+	cout << "================================================" << endl;
+	cout << "Enter the following information to create an account: " << endl;
+	cout << "1.ID : " << endl;
+	cout << "2.Name : " << endl;
+	cout << "3.Email : " << endl;
+	cout << "4.Password : " << endl;
+	cout << "5.Year Of Birth : " << endl;
+	cout << "6.Select T to register as a Teacher " << endl;
+	cout << "  Select S to register as a Student " << endl;
+	cout << "  Press ""1"" to confirm information and log in " << endl;
+	cout << "  Press ""2"" to log in" << endl;
+	cout << "  Press ""3"" to exit " << endl;
+	cout << "================================================" << endl;
+	cout << endl;
+	string ch;
+	cout << "Choice : ";
+	getline(cin, ch);
+	while (ch != "T" && ch != "S" && ch != "t" && ch != "s") {
+		cout << "Invalid input, please enter again : ";
+		getline(cin, ch);
+	}
+	if (ch == "T" || ch == "t") {
+		Teacher teacher;
+		cin >> teacher;
+		TeacherAccountData.insert({ teacher.getEmail(), teacher.getPassword() });
+		teacher.printInformation();
+	}
+	else if (ch == "S" || ch == "s") {
+		Student student;
+		cin >> student;
+		StudentAccountData.insert({ student.getEmail(), student.getPassword() });
+		student.printInformation();
+	}
+	else {
+		cout << "Exit successfully !" << endl;
+	}
+	cout << endl;
+	cout << endl;
+	cout << "================================================" << endl;
+	cout << "        Login to your account " << endl;
+	cout << "        Email : " << endl;
+	cout << "        Password : " << endl;
+	cout << "        Press ""E"" to login" << endl;
+	cout << "================================================" << endl;
+	string email, password;
+	cout << "Email : ";
+	getline(cin, email);
+	cout << "Password : ";
+	getline(cin, password);
+	if (AccountData.find(email) != AccountData.end() && AccountData[email] == password) {
+		if (StudentAccountData.find(email) != StudentAccountData.end() && StudentAccountData[email] == password
+			&& TeacherAccountData.find(email) == TeacherAccountData.end()) {
+			cout << "Login Successfully ! " << endl;
+		}
+		else if (TeacherAccountData.find(email) != TeacherAccountData.end() && TeacherAccountData[email] == password
+			&& StudentAccountData.find(email) == StudentAccountData.end()) {
+			cout << "Login Successfully ! " << endl;
+		}
+	}
+}
+
 int main()
 {
 	srand(time(NULL));
